b244bafc6f846c784f4cbdbd43de499ef4d8e287
Victor Moran <rvictor1991@hotmail.com>
2015-04-20 17:50:57 -0500

    Distance demo without Fix calculation

---
+++ ProyectosAndroid/Unidad2Laboratorio1/app/src/main/java/ejemplos/upc/org/unidad2laboratorio1/LeDeviceListAdapter.java
@@ -0,0 +1,119 @@
+package ejemplos.upc.org.unidad2laboratorio1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.Utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Victor Moran on 15/04/2015.
+ */
+public class LeDeviceListAdapter extends BaseAdapter {
+
+    private ArrayList<Beacon> beacons;
+    private LayoutInflater inflater;
+
+    public LeDeviceListAdapter(Context context) {
+        this.inflater = LayoutInflater.from(context);
+        this.beacons = new ArrayList<Beacon>();
+    }
+
+    public void replaceWith(Collection<Beacon> newBeacons) {
+        this.beacons.clear();
+        this.beacons.addAll(newBeacons);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return beacons.size();
+    }
+
+    @Override
+    public Beacon getItem(int position) {
+        return beacons.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        view = inflateIfRequired(view, position, parent);
+        bind(getItem(position), view);
+        return view;
+    }
+
+    private void bind(Beacon beacon, View view) {
+        ViewHolder holder = (ViewHolder) view.getTag();
+        holder.macTextView.setText(String.format("Name: %s (%.2fm)", beacon.getMacAddress(), Utils.computeAccuracy(beacon)));
+        holder.majorTextView.setText("Major: " + beacon.getMajor());
+        holder.minorTextView.setText("Minor: " + beacon.getMinor());
+        holder.measuredPowerTextView.setText("MPower: " + beacon.getMeasuredPower());
+        holder.rssiTextView.setText("RSSI: " + beacon.getRssi());
+        holder.distanceTextView.setText("Distance: " + calculateDistance(beacon));
+    }
+
+    private View inflateIfRequired(View view, int position, ViewGroup parent) {
+        if (view == null) {
+            view = inflater.inflate(R.layout.device_item, null);
+            view.setTag(new ViewHolder(view));
+        }
+        return view;
+    }
+
+    static class ViewHolder {
+        final TextView macTextView;
+        final TextView majorTextView;
+        final TextView minorTextView;
+        final TextView measuredPowerTextView;
+        final TextView rssiTextView;
+        final TextView distanceTextView;
+
+        ViewHolder(View view) {
+            macTextView = (TextView) view.findViewWithTag("mac");
+            majorTextView = (TextView) view.findViewWithTag("major");
+            minorTextView = (TextView) view.findViewWithTag("minor");
+            measuredPowerTextView = (TextView) view.findViewWithTag("mpower");
+            rssiTextView = (TextView) view.findViewWithTag("rssi");
+            distanceTextView = (TextView) view.findViewWithTag("distance");
+        }
+    }
+
+    private static double calculateDistance(Beacon beacon) {
+
+        int txPower = beacon.getMeasuredPower();
+        int rssi = beacon.getRssi();
+        double A,B,C;
+
+
+        //constant based on Nexus 4
+        A = 0.89976;//0.89976
+        B = 7.7095; // 7.7095
+        C = 0.111; //0.111
+        if (rssi == 0) {
+            return -1.0; // if we cannot determine distance, return -1.
+        }
+
+        double ratio = rssi*1.0/txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio,10);
+        }
+        else {
+            //double accuracy =  (A)*Math.pow(ratio,B) + C;
+            //double accuracy = Math.pow(10d,((double)txPower-rssi)/(10*2));
+            double accuracy = Math.pow(10d,((double)-62.72-rssi)/(2.2853));
+            return accuracy;
+        }
+    }
+}
---
+++ ProyectosAndroid/Unidad2Laboratorio1/app/src/main/java/ejemplos/upc/org/unidad2laboratorio1/PrimerActivity.java
@@ -0,0 +1,144 @@
+package ejemplos.upc.org.unidad2laboratorio1;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Victor Moran on 13/04/2015.
+ */
+public class PrimerActivity extends Activity {
+
+    private static final String TAG = PrimerActivity.class.getSimpleName();
+
+    public static final String EXTRAS_TARGET_ACTIVITY = "extrasTargetActivity";
+    public static final String EXTRAS_BEACON = "extrasBeacon";
+
+    private static final int REQUEST_ENABLE_BT = 1234;
+    private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
+    private BeaconManager beaconManager;
+    private LeDeviceListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primero);
+
+        adapter = new LeDeviceListAdapter(this);
+        ListView list = (ListView)findViewById(R.id.device_list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(createOnItemClickListener());
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
+                // Note that results are not delivered on UI thread.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Note that beacons reported here are already sorted by estimated
+                        // distance between device and beacon.
+                        adapter.replaceWith(beacons);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        beaconManager.disconnect();
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if device supports Bluetooth Low Energy.
+        if (!beaconManager.hasBluetooth()) {
+            Toast.makeText(this, "Device does not have Bluetooth Low Energy", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // If Bluetooth is not enabled, let user enable it.
+        if (!beaconManager.isBluetoothEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1234);
+        } else {
+            connectToService();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        try {
+            beaconManager.stopRanging(new Region("rid", null, null, null));
+        } catch (RemoteException e) {
+            Log.d(TAG, "Error while stopping ranging", e);
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234) {
+            if (resultCode == Activity.RESULT_OK) {
+                connectToService();
+            } else {
+                Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void connectToService() {
+        adapter.replaceWith(Collections.<Beacon>emptyList());
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
+                } catch (RemoteException e) {
+                    Toast.makeText(PrimerActivity.this, "Cannot start ranging, something terrible happened",
+                            Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "Cannot start ranging", e);
+                }
+            }
+        });
+    }
+
+    private AdapterView.OnItemClickListener createOnItemClickListener() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (getIntent().getStringExtra(EXTRAS_TARGET_ACTIVITY) != null) {
+                    try {
+                        Class<?> clazz = Class.forName(getIntent().getStringExtra(EXTRAS_TARGET_ACTIVITY));
+                        Intent intent = new Intent(PrimerActivity.this, clazz);
+                        intent.putExtra(EXTRAS_BEACON, adapter.getItem(position));
+                        startActivity(intent);
+                    } catch (ClassNotFoundException e) {
+                        Log.e(TAG, "Finding class by name failed", e);
+                    }
+                }
+            }
+        };
+    }
+}
